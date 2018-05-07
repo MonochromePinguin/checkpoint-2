1ec4c5b54d5440c22e6db36ee1051a13e6d25277
monochromePinguin <jaimaille.com@gmail.com>
2018-05-08 00:41:43 +0200

    addition of an "add planet" boostrap modal and some js code to handle it

--- public/js/BeastEdition.js
+++ public/js/BeastEdition.js
@@ -1,15 +1,62 @@
 $(document).ready( function(){
     $planetSelector = $('#planet');
-    $newPlanetOption = $('#option-create-new');
+    $dialog = $('#planet-creation-dialog');
+    $planetName = $('#new-planet-name');
+    $sendBtn = $('#new-planet-send-btn');
 
-    $planetSelector.append( new Option('Nouvelle planète ...', 'new-planet') );
 
+    function inhibitSendBtn() {
+        $sendBtn.attr('disabled', '');
+        $sendBtn.prop('title', 'Il faut d\'abord rentrer un nom de planète');
+    }
+
+
+    // hide the dialog and clear the input field on cancel
+    $('#new-planet-cancel-btn').click( function() {
+        $planetSelector.val( $planetSelector.children('option').first().val() );
+
+        $dialog.modal('hide');
+        $planetName.val('');
+        inhibitSendBtn();
+    });
+
+
+    //(de)inhibit the send button depending on name field content
+    $planetName.keyup( function() {
+        if ( '' != $planetName.val() ) {
+            $sendBtn.removeAttr('disabled');
+            $sendBtn.prop('title', 'cliquer pour envoyer la demande de création');
+        } else
+            inhibitSendBtn();
+    });
+
+
+    //show the planet creation dialog on **change** only ...
+//TODO: show it anytime the option is clicked ...
     $planetSelector.change( function() {
-
         //was the "nouvelle planète ..." option selected ?
-        if ( $newPlanetOption.selected ) {
-console.log('sélected');
+        $optId = $('#planet option:selected').attr('id');
+
+        if ( $optId == 'option-create-new' ) {
+            //Yes, we can show the planet creation form
+            $dialog.modal('show');
+            //set focus on the field
+            $dialog.on('shown.bs.modal', function () {
+                    $planetName.trigger('focus');
+            });
         }
     });
 
+
+    //on click, send a request to add a new planet through AJAX
+    $sendBtn.click( function() {
+        $dialog.modal('hide');
+
+        $.get( '/planets/ajax?createNew ' + $planetName.val(''),
+                function(data, status){
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+
+
+    })
 })
